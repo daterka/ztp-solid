1ee308ec2697f0ce64fa34eec586728417a4de3e
dan.te <fallowdeer.mail@gmail.com>
2020-03-29 14:00:21 +0200

    Initial commit

---
+++ src/Solver.java
@@ -0,0 +1,14 @@
+import javax.jws.WebService;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet(name = "servletNNNNNN")
+public class Solver extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
